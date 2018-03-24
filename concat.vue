71900dee64598b59026c485b0dd085cb1000d20d
Sofia <danikazsf@gmail.com>
2018-03-24 19:47:06 +0800

    some practices

---
+++ concat.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    methods: {
+        // eg1
+        function concat() {
+            const arr = Array.prototype.slice.apply(arguments);
+            arr1 = arr[0];
+            arr.splice(0, 1);
+            arr.forEach((item) => {
+                if (Array.isArray(item)) {
+                    arr1.push(...item);
+                } else {
+                    arr1.push(item);
+                }
+            });
+            return arr1;
+        },
+        // eg2
+        function concat() {
+            const [
+                resArr,
+                ...restArr
+            ] = Array.prototype.slice.apply(arguments);
+            restArr.forEach((item) => {
+                if (Array.isArray(item)) {
+                    resArr.push(...item);
+                } else {
+                    resArr.push(item);
+                }
+            });
+            return resArr;
+        },
+    },
+};
+</script>
+
---
+++ difference.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    methods: {
+        var arr1 = [1,2,3,4,5,6,7];
+        var arr2 = [1,3,4,5,8,9,10,22,1,2];
+        var arr3 = [];
+        arr1.forEach((item) => {
+              if (!arr2.includes(item)) {
+                   arr3.push(item);
+              }
+        });
+        arr2.forEach((item) => {
+            if (!arr1.includes(item)) {
+                 arr3.push(item);
+           }
+        });
+    },
+};
+</script>
