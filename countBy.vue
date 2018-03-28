a94fd7d78c5ad8188f739b9915b57b34d30b4dc3
Sofia <danikazsf@gmail.com>
2018-03-28 17:27:41 +0800

    union and countBy

---
+++ countBy.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    methods: {
+        // countBy([1,2,3,4,5,6,7], item => (item % 2 === 0)); => {false: 4, true: 3}
+        // countBy([1.2, 3.5, 6.7, 1.1, 6.2], item => (Math.floor(item))); => {1:2, 3:1, 6:2}
+        // countBy(['one', 'two', 'three', 'ssss'], 'length');  => {3:2, 4:1, 5:1}
+        // countBy([{a: 1, b:22}, {a:2, b: 21}, {a:1, b: 12}, {a:1, b: 12}], 'a')  => {1:3, 2:1}
+
+        function countBy(arr, func) {
+            var res=[], obj = {}, k;
+            if (typeof func === 'function') {
+                arr.forEach((item) => {
+                    res.push(func(item));
+                });
+                res.forEach((val) => {
+                    k = val;
+                    if(obj[k])
+                        obj[k]++;
+                    else
+                        obj[k] = 1;
+                })
+            } else if (typeof func === 'string') {
+                arr.forEach((item) => {
+                    k = item[func];
+                    if(obj[k])
+                        obj[k]++;
+                    else
+                        obj[k] = 1;
+                })
+            }
+
+            return obj;
+        }
+    },
+};
+</script>
---
+++ union.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    methods: {
+        // 只适用数字 union(union([2, 1], [4, 2], [1, 2]))
+        function union() {
+            const [...arr] = Array.prototype.slice.apply(arguments);
+            const flatten = [].concat(...arr).sort();
+            const re = [flatten[0]];
+            for(var i = 1; i < flatten.length; i++){
+                if( flatten[i] !== re[re.length-1]){
+                    re.push(flatten[i]);
+                }
+            }
+            return re;
+        }
+        // union([{a:1}, {b:1}], [{a:1}, {b:2}], [{a:1}]) => res=[{a:1}, {b:1}, {b:2}]
+        // union(union([2, 1], [4, 2], [1, 2])) => res=[1,2,4]
+        function union() {
+            const [...arr] = Array.prototype.slice.apply(arguments);
+            const flatten = [].concat(...arr);
+            let obj = {}, res = [];
+            flatten.forEach((item) => {
+                k = JSON.stringify(item);
+                if(obj[k])
+                    obj[k]++;
+                else
+                    obj[k] = 1;
+            })
+            Object.keys(obj).forEach((item) => {
+                res.push(JSON.parse(item));
+            })
+            return res;
+        }
+    },
+};
+</script>
